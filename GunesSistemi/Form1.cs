b4a605ab00e942fce14751b2ca44fdafcfead929
Cagil ALSAC <cagil.alsac@bilgeadam.com>
2022-10-01 13:07:17 +0300

    vFinal

--- GunesSistemi/Form1.cs
+++ GunesSistemi/Form1.cs
@@ -1,3 +1,4 @@
+using GunesSistemi.Models;
 using GunesSistemi.Models.Bases;
 using GunesSistemi.Services;
 using GunesSistemi.Services.Bases;
@@ -25,7 +26,25 @@ namespace GunesSistemi
         {
             int id = Convert.ToInt32(dgvGokCisimleri.SelectedRows[0].Cells[0].Value);
             GokCismiBase gokCismi = _servis.DetayGetir(id);
-            MessageBox.Show($"Id: {gokCismi.Id}\nAdý: {gokCismi.Adi}\nYarý Çap: {gokCismi.YariCap}");
+
+            //MessageBox.Show(nameof(GokCismiBase)); // "GokCismiBase"
+
+            if (gokCismi.GetType() == typeof(Yildiz))
+            {
+                Yildiz yildiz = gokCismi as Yildiz; // gokCismi as Yildiz: (Yildiz)gokCismi
+                MessageBox.Show($"Id: {yildiz.Id}\nAdý: {yildiz.Adi}\nYarý Çap: {yildiz.YariCap}\nSýcaklýk: {yildiz.SicaklikC} C ({yildiz.SicaklikF} F)", nameof(Yildiz));
+            }
+            else if (gokCismi.GetType() == typeof(Gezegen))
+            {
+                Gezegen gezegen = gokCismi as Gezegen;
+                string yasam = gezegen.YasamVarMi ? "Var" : "Yok";
+                MessageBox.Show($"Id: {gezegen.Id}\nAdý: {gezegen.Adi}\nYarý Çap: {gezegen.YariCap}\nYaþam: {yasam}", nameof(Gezegen));
+            }
+            else
+            {
+                Uydu uydu = gokCismi as Uydu;
+                MessageBox.Show($"Id: {uydu.Id}\nAdý: {uydu.Adi}\nYarý Çap: {uydu.YariCap}", nameof(Uydu));
+            }
         }
     }
 }
